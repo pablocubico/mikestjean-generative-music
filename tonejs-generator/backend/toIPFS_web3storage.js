0415a447def24c2095832d09e230b0bdd190c5df
Pablo Cuadrado <pablocuadrado@gmail.com>
2022-12-13 12:42:42 -0300

    switching from pinata cloud to web3.storage

---
+++ tonejs-generator/backend/toIPFS_web3storage.js
@@ -0,0 +1,35 @@
+import process from "process";
+import minimist from "minimist";
+import { Web3Storage, getFilesFromPath } from "web3.storage";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const token = process.env.WEB3_STORAGE_TOKEN;
+
+async function main() {
+  const args = minimist(process.argv.slice(2));
+
+  if (!token) {
+    return console.error(
+      "A token is needed. You can create one on https://web3.storage"
+    );
+  }
+
+  // if (args._.length < 1) {
+  //   return console.error("Please supply the path to a file or directory");
+  // }
+
+  const path = "dist/index.html";
+
+  const storage = new Web3Storage({ token });
+  const files = [];
+
+  const pathFiles = await getFilesFromPath(path);
+  files.push(...pathFiles);
+
+  console.log(`Uploading ${files.length} files`);
+  const cid = await storage.put(files);
+  console.log("Content added with CID:", cid);
+}
+
+main();
